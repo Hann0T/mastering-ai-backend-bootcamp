02ba416b940e9184dfab31c95322ccb7d67cbb66
hann0t <hansolivas1@gmail.com>
2026-04-01 17:32:35 -0500

    refactor: logging.

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
-const express = require('express')
+import fs from 'fs';
+import path from 'path';
+import express from 'express';
+import morgan from 'morgan';
+
 const app = express()
 const port = 3000
+
+const logPath = new URL('./logs/access.log', import.meta.url);
+const stream = fs.createWriteStream(logPath, { flags: 'a' });
+
+app.use(morgan('dev'));
+// app.use(morgan(':method :url :status :response-time ms', { stream }));
+app.use(morgan('combined', { stream }));
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
